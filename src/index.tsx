580ac91879d92d8bfb01593a4c0e80a7e7191df3
Gustavo Duarte <gduartemenezes@gmail.com>
2021-11-04 15:45:51 -0300

    feat: create simple server with miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createServer } from "miragejs";
 import { App } from "./App";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "",
+          amount: 50,
+          categoty: "",
+        },
+      ];
+    });
+    this.post();
+    this.put();
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
