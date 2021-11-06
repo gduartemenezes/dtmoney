b32019a3b818a49418dde691173a1a298b172928
Gustavo Duarte <gduartemenezes@gmail.com>
2021-11-06 14:53:27 -0300

    feat: add and style closeModal button

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,7 @@
 import Modal from "react-modal";
 
 import { Container } from "./styles";
-
+import closeImg from "../../assets/close.svg";
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -18,8 +18,15 @@ export function NewTransactionModal({
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar Modal" />
+      </button>
       <Container>
-        <h2>Cadastrar nova transação</h2>
+        <h2>Cadastrar transação</h2>
 
         <input type="text" placeholder="Título" />
         <input type="number" placeholder="Valor" />
