26c244084e591bb160684facc34f2a8e0888e2cd
Gustavo Duarte <gduartemenezes@gmail.com>
2021-11-06 14:04:48 -0300

    feat: recieve external props to handle modal opening

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import logoImg from "../../assets/Logo.svg";
 import { Container, Content } from "./styles";
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dtMoney" />
-        <button type="button"> Nova transação</button>
+        <button onClick={onOpenNewTransactionModal} type="button">
+          {" "}
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
